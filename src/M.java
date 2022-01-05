032b7b6c772d583ff062ed0ecb0162cba539ff7a
ivan.bylinovich <ib6020607@gmail.com>
2022-01-05 11:04:22 +0300

    <first changing>

--- src/M.java
+++ src/M.java
@@ -1,11 +1,5 @@
 public class M {
     public static void main(String[] args) {
 
-        try {
-            throw new UnsupportedOperationException();
-        } catch(Throwable t) {
-            System.out.print("1");
-        }
-
     }
 }
